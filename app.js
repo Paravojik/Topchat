71f8ae838eda7a3663e2b132cbba8f5c4333d0f2
Paravojik <86243107+Paravojik@users.noreply.github.com>
2022-11-29 16:43:25 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+let PORT = 3000;
+let io =  require('socket.io')(http);
+let userCount=0
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+io.on('connection', function(socket){
+    console.log('new user connected');
+    userCount++
+    io.emit('getStatus',userCount)
+    socket.on('chat message',function(msg){
+        io.emit('chat message', msg)
+    })
+    socket.on('new user',function(user){
+        io.emit('new user', user)
+        console.log('Msg sended by: ',user)
+    })
+    socket.on('disconnect',function(){
+        console.log('user disconect')
+        userCount--
+        io.emit('getStatus',userCount)
+    })
+})
+
+http.listen(PORT, function(){
+    console.log(`server listening on port: ${PORT}`);
+});
